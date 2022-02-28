8a0d803c810262772bae10355088e9f956f2168e
techishant <ishantsh12234@gmail.com>
2022-02-28 22:27:58 +0530

    Made a new project animal-classification

---
+++ projects/animal-classification/animals.py
@@ -0,0 +1,2 @@
+print("Animals: The word animal comes from the Latin animalis, meaning 'having breath', 'having soul' or 'living being'. The biological definition includes all members of the kingdom Animalia. In colloquial usage, the term animal is often used to refer only to nonhuman animals")
+
