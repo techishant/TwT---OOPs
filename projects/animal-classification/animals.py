fa4c048459436c4a3667bf6cc36cb615903ef33f
techishant <ishantsh12234@gmail.com>
2022-03-01 15:36:20 +0530

    Few additions in the info of a particular

--- projects/animal-classification/animals.py
+++ projects/animal-classification/animals.py
@@ -1,2 +1,17 @@
 print("Animals: The word animal comes from the Latin animalis, meaning 'having breath', 'having soul' or 'living being'. The biological definition includes all members of the kingdom Animalia. In colloquial usage, the term animal is often used to refer only to nonhuman animals")
 
+class Animalia():
+    def __init__(self,
+                scientific_name, 
+                commonNames, 
+                males, females, 
+                lifespan,
+                kingdom, 
+                phylum, 
+                ani_Class, 
+                order,
+                family,
+                genus,
+                external_links
+                ) -> None:
+        pass
